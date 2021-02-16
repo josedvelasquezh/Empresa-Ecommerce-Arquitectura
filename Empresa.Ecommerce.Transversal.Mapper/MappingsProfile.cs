de2e6da490b32fb2464db56ef1030d396859e0dc
J. Daniel Velasquez <jvelasquez@thefactoryhka.com>
2021-02-16 18:38:49 -0500

    Se crea el Mapeo de la clase Customer(Capa Dominio) a CustomerDTO (Capa Aplicacion)

---
+++ Empresa.Ecommerce.Transversal.Mapper/MappingsProfile.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using AutoMapper;
+using Empresa.Ecommerce.Domain.Entity;
+using Empresa.Ecommerce.Application.DTO;
+
+namespace Empresa.Ecommerce.Transversal.Mapper
+{
+    public class MappingsProfile : Profile
+    {
+        public MappingsProfile()
+        {
+            //Si todos los atributos se llaman igual en la clase de Aplicacion CustomerDTO y la Domain Entity Customer
+            CreateMap<Customers, CustomersDTO>().ReverseMap();
+
+            //En caso de que los atributos no se llaman igual, se puede aplicar atributo por atributo
+
+            /*
+             CreateMap<Customers, CustomersDTO>().ReverseMap()
+                .ForMember(
+                destination => destination.CustomerId,
+                source => source.MapFrom(src => src.CustomerId))
+                .ForMember(
+                destination => destination.CompanyName,
+                source => source.MapFrom(src => src.CompanyName)).ReverseMap();
+            */
+        }
+    }
+}
