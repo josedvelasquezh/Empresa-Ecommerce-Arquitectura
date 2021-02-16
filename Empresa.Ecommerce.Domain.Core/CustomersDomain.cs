81b318a8bbe87f2104473678b7322070855f52ff
J. Daniel Velasquez <jvelasquez@thefactoryhka.com>
2021-02-16 14:45:03 -0500

    Se trabaja en la capa de dominio para implementar las logicas de negocio utilizando una inferface y utilizandos los metodos de la capa infraestructura de Customers

---
+++ Empresa.Ecommerce.Domain.Core/CustomersDomain.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Empresa.Ecommerce.Domain.Entity;
+using Empresa.Ecommerce.Domain.Interface;
+using Empresa.Ecommerce.Infraestruture.Interface;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Empresa.Ecommerce.Domain.Core
+{
+    //Logicas de reglas de negocio van aqui para ir a la capa de infraestructura
+    public class CustomersDomain : ICustomersDomain
+    {
+        //Tiene que consumir una interface de la capa inferior
+        private readonly ICustomerRepository _customerRepository;
+
+        //Inyeccion de dependencias
+        public CustomersDomain(ICustomerRepository customerRepository)
+        {
+            //Le asignamos el valor de nuestro constructor
+            _customerRepository = customerRepository;
+        }
+
+        #region Metodos Sincronos
+
+        public bool Insert(Customers customers)
+        {
+            return _customerRepository.Insert(customers);
+        }
+
+        public bool Update(Customers customers)
+        {
+            return _customerRepository.Update(customers);
+        }
+
+        public bool Delete(String customerId)
+        {
+            return _customerRepository.Delete(customerId);
+        }
+
+        public Customers Get(String customerId)
+        {
+            return _customerRepository.Get(customerId);
+        }
+
+        public IEnumerable<Customers> GetAll()
+        {
+            return _customerRepository.GetAll();
+        }
+
+        #endregion
+
+        #region Metodos Asincronos
+
+        public async Task<bool> InsertAsync(Customers customers)
+        {
+            return await _customerRepository.InsertAsync(customers);
+        }
+
+        public async Task<bool> UpdateAsync(Customers customers)
+        {
+            return await _customerRepository.UpdateAsync(customers);
+        }
+
+        public async Task<bool> DeleteAsync(String customerId)
+        {
+            return await _customerRepository.DeleteAsync(customerId);
+        }
+
+        public async Task<Customers> GetAsync(String customerId)
+        {
+            return await _customerRepository.GetAsync(customerId);
+        }
+
+        public async Task<IEnumerable<Customers>> GetAllAsync()
+        {
+            return await _customerRepository.GetAllAsync();
+        }
+
+        #endregion
+    }
+}
---
+++ Empresa.Ecommerce.Domain.Interface/ICustomersDomain.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Empresa.Ecommerce.Domain.Entity;
+using System.Threading.Tasks;
+
+namespace Empresa.Ecommerce.Domain.Interface
+{
+    //Son las mismas operaciones que ya realizamos en la interface ICustomerRepository
+    public interface ICustomersDomain
+    {
+        #region Metodos Sincronos
+        bool Insert(Customers customer);
+        bool Update(Customers customer);
+        bool Delete(String customerId);
+        Customers Get(String customerId);
+        IEnumerable<Customers> GetAll();
+
+        #endregion
+
+        #region Metodos Asincronos
+        Task<bool> InsertAsync(Customers customer);
+        Task<bool> UpdateAsync(Customers customer);
+        Task<bool> DeleteAsync(String customerId);
+        Task<Customers> GetAsync(String customerId);
+        Task<IEnumerable<Customers>> GetAllAsync();
+
+        #endregion
+    }
+}
