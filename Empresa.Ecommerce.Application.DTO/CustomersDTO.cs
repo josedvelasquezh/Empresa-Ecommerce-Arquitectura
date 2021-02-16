c2c36161f3d9549ef83f548d0258523409752753
J. Daniel Velasquez <jvelasquez@thefactoryhka.com>
2021-02-16 18:37:41 -0500

    Se crea clase DTO para la clase a mostrar a nivel de api que no necesariamente son los mismos valores de la tabla

---
+++ Empresa.Ecommerce.Application.DTO/CustomersDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Empresa.Ecommerce.Application.DTO
+{
+    public class CustomersDTO
+    {
+        public String CustomerId { get; set; }
+        public String CompanyName { get; set; }
+        public String ContactName { get; set; }
+        public String ContactTitle { get; set; }
+        public String Address { get; set; }
+        public String City { get; set; }
+        public String Region { get; set; }
+        public String Country { get; set; }
+        public String Phone { get; set; }
+    }
+}
