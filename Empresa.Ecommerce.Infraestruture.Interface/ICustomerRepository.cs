46ea81904acfc44a62036ffa491716246d0d9a81
J. Daniel Velasquez <jvelasquez@thefactoryhka.com>
2021-02-16 13:48:28 -0500

    Creacion de la interface de Customer con los metodos definidos

---
+++ Empresa.Ecommerce.Infraestruture.Interface/ICustomerRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Empresa.Ecommerce.Domain.Entity;
+
+namespace Empresa.Ecommerce.Infraestruture.Interface
+{
+    //Interface de las operaciones para ser implementados
+    public interface ICustomerRepository
+    {
+        #region Metodos Sincronos
+
+        bool Insert(Customer customer);
+        bool Update(Customer customer);
+        bool Delete(String customerId);
+        Customer Get(String customerId);
+        IEnumerable<Customer> GetAll();
+
+        #endregion
+
+        #region Metodos Asincronos
+        Task<bool> InsertAsync(Customer customer);
+        Task<bool> UpdateAsync(Customer customer);
+        Task<bool> DeleteAsync(String customerId);
+        Task<Customer> GetAsync(String customerId);
+        Task<IEnumerable<Customer>> GetAllAsync();
+
+        #endregion
+    }
+}
