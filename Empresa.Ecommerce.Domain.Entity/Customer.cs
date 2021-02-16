4e0553a9ccded416ff5a5ddd7644d98ec2fc914e
J. Daniel Velasquez <jvelasquez@thefactoryhka.com>
2021-02-16 13:45:27 -0500

    Se define la clase Customer para trabajar en la tabla Clientes

---
+++ Empresa.Ecommerce.Domain.Entity/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Empresa.Ecommerce.Domain.Entity
+{
+    public class Customer
+    {
+        public String CustomerID { get; set; }
+        public String CompanyName { get; set; }
+        public String ContactName { get; set; }
+        public String ContactTitle { get; set; }
+        public String Address { get; set; }
+        public String City { get; set; }
+        public String Region { get; set; }
+        public String PostalCode { get; set; }
+        public String Country { get; set; }
+        public String Phone { get; set; }
+        public String Fax { get; set; }
+    }
+}
