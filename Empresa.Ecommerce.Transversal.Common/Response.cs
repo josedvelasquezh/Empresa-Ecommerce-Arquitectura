e52bcc14317750a335e14e09dc5ef74898df9366
J. Daniel Velasquez <jvelasquez@thefactoryhka.com>
2021-02-16 18:36:54 -0500

    Se crea en Transversal un Response Generico que tiene un tipo de Dato T para retornar valores Variables

---
+++ Empresa.Ecommerce.Transversal.Common/Response.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Empresa.Ecommerce.Transversal.Common
+{
+    public class Response<T>
+    {
+        public T Data { get; set; }
+        public bool IsSuccess { get; set; }
+        public String Message { get; set; }
+    }
+}
