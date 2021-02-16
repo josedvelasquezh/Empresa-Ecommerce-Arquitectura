1d2273dec3b3a348bdc9ad48fc453dc26606ccc4
J. Daniel Velasquez <jvelasquez@thefactoryhka.com>
2021-02-16 13:33:01 -0500

    Se crea el archivo de Connection para apertura de la conexion

--- Empresa.Ecommerce.Infraestructure.Data/ConnectionFactory.cs
+++ Empresa.Ecommerce.Infraestructure.Data/ConnectionFactory.cs
@@ -1,9 +1,33 @@
 ﻿using System;
+using Empresa.Ecommerce.Transversal.Common;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace Empresa.Ecommerce.Infraestructure.Data
 {
-    public class ConnectionFactory
+    public class ConnectionFactory : IConnectionFactory
     {
+        private readonly IConfiguration _configuration;
+
+        public ConnectionFactory(IConfiguration configuration)
+        {
+            //Inyeccion de dependencia a traves de constructor
+            _configuration = configuration;
+        }
+
+        public IDbConnection GetConnection
+        {
+            get
+            {
+                var sqlConnection = new SqlConnection();
+                if (sqlConnection == null) return null;
+
+                sqlConnection.ConnectionString = _configuration.GetConnectionString("NorthwindConnection");
+                sqlConnection.Open();
+                return sqlConnection;
+            }
+        }
 
     }
 }
